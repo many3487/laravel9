2ce12f85063813768665a6fd32454f55d34ae225
many3487 <manuelja@hotmail.es>
2023-09-11 09:31:14 -0500

    crear

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
 
 class PostController extends Controller
 {
@@ -15,16 +18,36 @@ class PostController extends Controller
         ]);
         
     }
+
     public function destroy(Post $post)
     {
         $post->delete();
         return back();
         
     }
+
     public function create(Post $post)
     {
-        return view('posts.create',['post' =>$post]);//se pasa un elemento vacio
+        return view('posts.create',['post' => $post]);//se pasa un elemento
     }
+
+    public function store(Request $request)
+    {
+        $post = $request->user()->posts()->create([
+            'title' => $title = $request -> title, 
+            'slug' => Str::slug($title), 
+            'body' => $request -> body, 
+        ]);
+
+        return redirect() -> route('posts.edit',$post);//se pasa un elemento
+    }
+    
+    public function update(Request $request, Post $post)
+    {
+    
+        return redirect()->route('posts.index');
+    }
+
     public function edit(Post $post)
     {
         return view('posts.edit',['post' =>$post]);
